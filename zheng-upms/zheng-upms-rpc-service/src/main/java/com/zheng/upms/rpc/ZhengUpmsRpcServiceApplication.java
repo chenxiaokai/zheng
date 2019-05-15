22524d5064677ca6dd1a7489aea792312c6acf77
chenxiaokai <324713943@qq.com>
2019-05-15 17:48:53 +0800

    增加代碼注釋

--- zheng-upms/zheng-upms-rpc-service/src/main/java/com/zheng/upms/rpc/ZhengUpmsRpcServiceApplication.java
+++ zheng-upms/zheng-upms-rpc-service/src/main/java/com/zheng/upms/rpc/ZhengUpmsRpcServiceApplication.java
@@ -7,6 +7,9 @@ package com.zheng.upms.rpc;
 public class ZhengUpmsRpcServiceApplication {
 
 	public static void main(String[] args) {
+		/*
+		按照dubbo官网解说，要使得Main能正确的启用Spring容器，要求spring配置文件存放至classpath*:META-INF/spring/*.xml路径底下
+		 */
 		com.alibaba.dubbo.container.Main.main(args);
 	}
 
